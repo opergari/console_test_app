fab94f8a6fa41ae46779d0bc8740ff4f2a32dc76
opergari <oper.gari.73@gmail.com>
2023-06-18 14:38:58 +0300

    add Task_2.py

--- Task_2.5.py
+++ Task_2.5.py
@@ -1,2 +1,5 @@
-num = input('Введите 3 числа: ')
-print(max(num))
+num_1 = float(input('Введите первое число: '))
+num_2 = float(input('Введите второе число: '))
+num_3 = float(input('Введите третье число: '))
+
+print(f'Максимальное чило: {max(num_1, num_2, num_3)}')
